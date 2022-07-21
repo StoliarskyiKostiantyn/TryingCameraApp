0c0fce55867d4f5ef6f8c56284f70cf9e3b9928a
Костя Столярский <saintkos117@gmail.com>
2022-07-21 15:27:43 +0300

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,102 @@
+import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import {
+  Button,
+  StyleSheet, Text, Touchable, TouchableOpacity, View
+} from 'react-native';
+import { Camera, PhotoFile, TakePhotoOptions, TakeSnapshotOptions, useCameraDevices, VideoFile } from 'react-native-vision-camera';
+import { BarcodeFormat, useScanBarcodes } from 'vision-camera-code-scanner';
+
+
+const App = () => {
+  const camera =  useRef<Camera>(null);
+  const [hasPermission, setHasPermission] = useState(false);
+  const devices = useCameraDevices();
+  const device = devices.back;
+  const [frameProcessor, barcodes] = useScanBarcodes([BarcodeFormat.QR_CODE], {
+    checkInverted: true,
+  });
+
+ useEffect(() => {
+    (async () => {
+      const status = await Camera.requestCameraPermission();
+      setHasPermission(status === 'authorized');
+    })();
+  }, []);
+  if (device == null) return 
+
+  console.log('hasPermission:',hasPermission)
+  console.log('barcodes:',barcodes)
+ 
+
+
+
+  const takePhoto = async () => {
+    try {
+      if (camera.current == null) throw new Error('Camera ref is null!');
+      console.log('Taking photo...');
+      const photo = await camera.current.takePhoto();
+      console.log('photo:',photo)
+    } catch (e) {
+      console.error('Failed to take photo!', e);
+    }
+  };
+
+
+  return (
+      <>
+        <Camera style={StyleSheet.absoluteFill}
+          device={device}
+          isActive={true} 
+          ref={camera}
+          photo={true}
+          frameProcessor={frameProcessor}
+          frameProcessorFps={5}/>
+           {/* {barcodes.map((barcode, idx) => (
+          <Text key={idx} style={styles.barcodeTextURL}>
+            {barcode.displayValue}
+          </Text>
+        ))} */}
+     <TouchableOpacity
+            style={styles.button}
+            onPress={takePhoto}  
+          >
+            <View style={styles.takePhotoOut}>
+              <View style={styles.takePhotoInner}></View>
+            </View>
+          </TouchableOpacity>
+     </>
+  )
+};
+
+const styles = StyleSheet.create({
+  barcodeTextURL: {
+    fontSize: 20,
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  button: { alignSelf: "center" },
+
+  takePhotoOut: {
+    borderWidth: 2,
+    borderColor: "white",
+    height: 50,
+    width: 50,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 50,
+  },
+
+  takePhotoInner: {
+    borderWidth: 2,
+    borderColor: "white",
+    height: 40,
+    width: 40,
+    backgroundColor: "white",
+    borderRadius: 50,
+  },
+});
+
+export default App;
+
+
---
+++ LoadingView.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+type Props = {}
+
+export const LoadingView = (props: Props) => {
+  return (
+    <Text>LoadingView</Text>
+  )
+}
